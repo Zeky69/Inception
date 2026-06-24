49d3182cbf5cae8fde5f7675ee287ad85fd22cbe
zakburak <zakburak@student.42.fr>
2026-06-24 14:10:31 +0200

    update SSL certificate generation subject to reflect correct location details

--- srcs/requirements/nginx/tools/generate_ssl.sh
+++ srcs/requirements/nginx/tools/generate_ssl.sh
@@ -14,7 +14,7 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/nginx.key \
         -out /etc/nginx/ssl/nginx.crt \
-        -subj "/C=FR/ST=IDF/L=Paris/O=42/CN=${DOMAIN_NAME}"
+        -subj "/C=FR/ST=Alsace/L=Mulhouse/O=42/CN=${DOMAIN_NAME}"
 
     chmod 600 /etc/nginx/ssl/nginx.key
     chmod 644 /etc/nginx/ssl/nginx.crt
